92f229461b012734577b3d103eef6bad1f0a7301
Kevin Chao <kevin.chao@mail.utoronto.ca>
2016-07-12 19:53:07 -0400

    Populate Airport model with city names using Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Auto populate Airport model with city names using Faker gem
+99.times do |n|
+  name  = Faker::Address.city
+  Airport.create!(name:  name)
+end
