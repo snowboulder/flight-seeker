f96c0a87c4813635769a5af1555ebdf9d41f0516
Kevin Chao <kevin.chao@mail.utoronto.ca>
2016-07-28 14:23:30 -0400

    Add Flight index page

---
+++ app/controllers/flights_controller.rb
@@ -0,0 +1,8 @@
+class FlightsController < ApplicationController
+
+  def index
+    @flights = Flight.all
+    @airports = Airport.all
+  end
+
+end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,9 @@
 class Flight < ApplicationRecord
   belongs_to :from_airport, :class_name => "Airport"
   belongs_to :to_airport, :class_name => "Airport"
+
+  def event_date_formatted
+    event_date.strftime("%m/%d/%Y")
+  end
+  
 end
