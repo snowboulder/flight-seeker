1033d7a696a08e2cfb913c9712b37cc535c4d00c
Kevin Chao <kevin.chao@mail.utoronto.ca>
2016-07-30 13:07:16 -0400

    Add number of passengers to Flight index search

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,9 +1,9 @@
 class Flight < ApplicationRecord
   belongs_to :from_airport, :class_name => "Airport"
   belongs_to :to_airport, :class_name => "Airport"
+  attr_accessor :quantity
 
   def event_date_formatted
     date.strftime("%m/%d/%Y")
   end
-
 end
