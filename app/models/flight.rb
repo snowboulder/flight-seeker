b27a8e5e1e88b14871f5ad8fd14907c050672387
Kevin Chao <kevin.chao@mail.utoronto.ca>
2016-08-01 00:03:39 -0700

    Add Search Results to Index page

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -8,13 +8,17 @@ class Flight < ApplicationRecord
   end
 
   def self.all_dates
-    all.collect { |f| f.formatted_date }.uniq
+    all.collect { |f| [f.formatted_date, f.date.to_date] }.uniq
+  end
+
+  def self.on_date(date)
+    where(date: date.beginning_of_day..date.end_of_day)
   end
 
   def self.search(params)
     if params[:search]
-      Flight.date.where(from_airport_id: params[:from_airport],
-                        to_airport_id: params[:to_airport])
+      Flight.on_date(params[:date].to_date)
+            .where(from_airport_id: params[:from_airport], to_airport_id: params[:to_airport])
             .includes(:from_airport, :to_airport)
     end
   end
