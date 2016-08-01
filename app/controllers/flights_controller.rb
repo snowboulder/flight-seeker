f6b2186fc481fcf3cfa06ec9fed7aa7f03c37bfa
Kevin Chao <kevin.chao@mail.utoronto.ca>
2016-07-31 19:03:55 -0700

    Improve Flight index data handling

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,8 +1,11 @@
 class FlightsController < ApplicationController
 
   def index
-    @flights = Flight.all
-    @airports = Airport.all
+    @airport_names = Airport.all.collect { |a| [a.name, a.id] }
+    @flight_dates = Flight.all_dates
+    @results = Flight.search(params)
+    #@flights = Flight.all
+    #@airports = Airport.all
   end
 
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,7 +3,19 @@ class Flight < ApplicationRecord
   belongs_to :to_airport, :class_name => "Airport"
   attr_accessor :quantity
 
-  def event_date_formatted
+  def formatted_date
     date.strftime("%m/%d/%Y")
+  end
+
+  def self.all_dates
+    all.collect { |f| f.formatted_date }.uniq
+  end
+
+  def self.search(params)
+    if params[:search]
+      Flight.date.where(from_airport_id: params[:from_airport],
+                        to_airport_id: params[:to_airport])
+            .includes(:from_airport, :to_airport)
+    end
   end
 end
